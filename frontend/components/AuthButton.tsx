75b1d5f05be433567e4233f418ccc656d4e027f0
delivops-app <delivops@gmail.com>
2025-09-04 17:53:34 +0200

    feat: add auth button component

---
+++ frontend/components/AuthButton.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import Link from 'next/link'
+import { useUser } from '@auth0/nextjs-auth0/client'
+
+export default function AuthButton() {
+  const { user } = useUser()
+
+  if (!user) {
+    return (
+      <Link
+        href="/api/auth/login"
+        className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
+      >
+        Se connecter
+      </Link>
+    )
+  }
+
+  return (
+    <Link
+      href="/api/auth/logout"
+      className="rounded bg-gray-600 px-4 py-2 text-white hover:bg-gray-700"
+    >
+      Se déconnecter
+    </Link>
+  )
+}
